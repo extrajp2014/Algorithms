1e2c5fd0e9a387f4597e62c7efca346430aefb49
ExtraJP2014 <extrajp2014@gmail.com>
2020-02-06 23:04:48 -0500

    fix final output for print statement

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -8,6 +8,7 @@ def recipe_batches(recipe, ingredients):
             ingredients[i] = 0
         ingredients[i] -= recipe[i]
         if ingredients[i] < 0:
+            ingredients[i] += recipe[i]
             return 0
     return recipe_batches(recipe, ingredients) + 1
 
@@ -15,5 +16,5 @@ if __name__ == '__main__':
     # Change the entries of these dictionaries to test 
     # your implementation with different inputs
     recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-    ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+    ingredients = { 'milk': 232, 'butter': 100, 'flour': 51 }
     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
